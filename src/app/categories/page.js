3579659e7c161b1b139ec7529acb0d8a47667ce3
Tommy-S05 <tommy-s05f@hotmail.com>
2023-06-06 21:02:19 -0400

    pagina categoria solamente anadida

---
+++ src/app/categories/page.js
@@ -0,0 +1,9 @@
+export default function Categories() {
+    return (
+        <main>
+            <section className={'md:py-10 pt-10'}>
+            
+            </section>
+        </main>
+    );
+}
