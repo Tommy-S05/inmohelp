5a8d043a965a22a0272cc347fccd2ff64f749902
AlberticoRD <alberticofd@gmail.com>
2023-06-16 20:32:53 -0400

    login terminado

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,18 +1,19 @@
 "use client";
+import "../borrar.css";
 export default function login() {
   return (
-    <section className="flex flex-col col items-center">
+    <section className="flex flex-col col items-center mt-32">
       <div class="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
         <form class="space-y-6" action="#">
-          <h5 class="text-xl font-medium text-gray-900 dark:text-white">
-            Sign in to our platform
+          <h5 class="flex text-xl justify-center font-medium text-gray-900 dark:text-white">
+            iniciar sesion
           </h5>
           <div>
             <label
               for="email"
               class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
             >
-              Your email
+              email
             </label>
             <input
               type="email"
@@ -28,7 +29,7 @@ export default function login() {
               for="password"
               class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
             >
-              Your password
+              contraseña
             </label>
             <input
               type="password"
@@ -61,14 +62,26 @@ export default function login() {
               href="#"
               class="ml-auto text-sm text-blue-700 hover:underline dark:text-blue-500"
             >
-              Lost Password?
+              ¿Olvidaste tu contraseña?
             </a>
           </div>
           <button
             type="submit"
-            class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            class="w-full text-white bg-primary hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             Login to your account
+          </button>
+          <button
+            type="submit"
+            class=" flex w-full justify-center  text-primary  border-2 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Sign In Whith Google{" "}
+            <img
+              className="ml-2"
+              src="assets/login/icons-google.svg"
+              height={20}
+              width={20}
+            />
           </button>
           <div class="text-sm font-medium text-gray-500 dark:text-gray-300">
             Not registered?{" "}
